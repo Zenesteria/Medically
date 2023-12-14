6d067815379adcdbf8119ae2f3dad08a98680d18
The Zenith <abdurrahman.aderinto@gmail.com>
2023-12-14 10:31:30 +0100

    added completions method for initial testing

--- app/api/chat-completion/route.ts
+++ app/api/chat-completion/route.ts
@@ -1,9 +1,17 @@
 import { NextResponse } from "next/server";
+import OpenAI from "openai";
 
 const POST = async (req:Request) => {
-    const url = "https://api.openai.com/v1/chat/completions";
+    const openai = new OpenAI()
+    // const url = "https://api.openai.com/v1/chat/completions";
     try {
-        
+        const completion = await openai.chat.completions.create({
+          messages: [
+            { role: "system", content: "You are a helpful assistant." },
+          ],
+          model: "gpt-3.5-turbo",
+        });
+        console.log(completion.choices[0])
     } catch (error) {
        return new NextResponse('Internal Error', {status:401});
     }
