001033027a8f55343e603779d2a1590304af57e5
The Zenith <abdurrahman.aderinto@gmail.com>
2023-12-14 15:19:58 +0100

    removed redundant code

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -6,46 +6,6 @@ interface LayoutProps {
   children: React.ReactNode;
 }
 
-import {MessageSquare, File, LayoutGrid, BarChart3, Settings, HelpCircle, LucideIcon} from 'lucide-react'
-
-
-
-const SideBarItems: {
-  Icon: LucideIcon;
-  name: string;
-  href: string;
-}[] = [
-  {
-    Icon: MessageSquare,
-    href: "/",
-    name: "AI Chat Helper",
-  },
-  {
-    Icon: File,
-    href: "/templates",
-    name: "Templates",
-  },
-  {
-    Icon: LayoutGrid,
-    href: "/projects",
-    name: "My Project",
-  },
-  {
-    Icon: BarChart3,
-    href: "/statistics",
-    name: "Statistics",
-  },
-  {
-    Icon: Settings,
-    href: "/settings",
-    name: "Settings",
-  },
-  {
-    Icon: HelpCircle,
-    href: "/FAQ",
-    name: "Updates & FAQ",
-  },
-];
 
 export default function layout({ children }: LayoutProps) {
   return (
