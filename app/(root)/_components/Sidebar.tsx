9a6fe935bacfc89314de7d3947fa96833a039d4a
The Zenith <abdurrahman.aderinto@gmail.com>
2023-12-14 12:59:54 +0100

    added sidebar

---
+++ app/(root)/_components/Sidebar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface SidebarProps{
+
+}
+
+export default function Sidebar({}:SidebarProps) {
+  return (
+    <div className='flex-[0.5]'>
+      
+    </div>
+  )
+}
