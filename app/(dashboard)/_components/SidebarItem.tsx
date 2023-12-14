5233e389775e4847cc94718e4046eb813bdfdab3
The Zenith <abdurrahman.aderinto@gmail.com>
2023-12-14 16:03:42 +0100

    onclick function added

--- app/(dashboard)/_components/SidebarItem.tsx
+++ app/(dashboard)/_components/SidebarItem.tsx
@@ -19,8 +19,11 @@ export default function SidebarItem({ href, Icon, name }: CompProps) {
     pathName == href ||
     pathName.startsWith(`/${href}`);
 
+  const OnClick = () => {
+    router.push(href)
+  }
   return (
-    <div className={cn("my-3 w-full flex items-center  p-4 rounded-md cursor-pointer duration-300 hover:bg-white/20 hover:text-cyan-300 text-white", isActive && 'bg-white/20 text-cyan-300')}>
+    <div onClick={OnClick} className={cn("my-3 w-full flex items-center  p-4 rounded-md cursor-pointer duration-300 hover:bg-white/20 hover:text-cyan-300 text-white", isActive && 'bg-white/20 text-cyan-300')}>
         <Icon/>
         <h1 className="text-[0.8rem] mx-2">
             {name}
