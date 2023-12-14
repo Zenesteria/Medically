9fa84ae3b2f9c5fd43e26e6247abd2ff90a6a794
The Zenith <abdurrahman.aderinto@gmail.com>
2023-12-14 08:56:23 +0100

    initial commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface LayoutProps{
+  children:React.ReactNode
+}
+
+export default function layout({children}:LayoutProps) {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
