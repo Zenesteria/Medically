852fc088fd67d6be1c22ca2f1f214717847df57e
The Zenith <abdurrahman.aderinto@gmail.com>
2023-12-14 13:01:28 +0100

    added sidebar to layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
+import Sidebar from "./_components/Sidebar";
 
-interface LayoutProps{
-  children:React.ReactNode
+interface LayoutProps {
+  children: React.ReactNode;
 }
 
-export default function layout({children}:LayoutProps) {
+export default function layout({ children }: LayoutProps) {
   return (
-    <div className='w-full h-screen bg-black p-4'>
-      {children}
+    <div className="w-full h-screen bg-black p-4 flex items-center">
+      <Sidebar/>
+      <div className="bg-white flex-1 rounded-xl h-full">{children}</div>
     </div>
-  )
+  );
 }
