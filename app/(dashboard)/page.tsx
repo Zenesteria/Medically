cb2f8fd54863ba352224bf4c0399f131be010974
The Zenith <abdurrahman.aderinto@gmail.com>
2023-12-14 15:23:46 +0100

    base layout added

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div className=''>
-      
+    <div className='flex w-full h-full items-center'>
+        <div className="flex-1 shadow-xl bg-white relative z-[100] h-full"></div>
+        <div className="flex-[0.3] h-full"></div>
     </div>
   )
 }
